eb11364bd2d85d1f77bd814c5169fa10a6194a22
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-03-31 08:04:13 -0300

    Mapeando as configurações de urls do app users com as configurações do project e adicionando o namespace, e configurando o ambiente de testes do Django

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class UsersTest(TestCase):
+
+    def test_login_view_url_is_correct(self):
+        url = reverse('users:login')
+        self.assertEqual("/users/",url)
